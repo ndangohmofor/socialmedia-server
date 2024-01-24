823b2fc03ed986519e87ac2304a084f8bc699e26
I M Ndangoh <37620979+ndangohmofor@users.noreply.github.com>
2024-01-24 06:35:10 -0500

    Add controller method for getUserFriends to user controller

--- controllers/users.js
+++ controllers/users.js
@@ -10,3 +10,22 @@ export const getUser = async (req, res) => {
     res.status(404).json({ error: err.message });
   }
 };
+
+export const getUserFriends = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const user = await User.findById(id);
+
+    const friends = await Promise.all(
+      user.friends.map((id) => User.findById(id))
+    );
+    const formatterFriends = friends.map(
+      ({ _id, firstName, lastName, occupation, location, picturePath }) => {
+        return { _id, firstName, lastName, occupation, location, picturePath };
+      }
+    );
+    res.status(200).json(formatterFriends);
+  } catch (err) {
+    res.status(404).json({ error: err.message });
+  }
+};
